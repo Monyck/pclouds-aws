fcaa539ba84bf83346f12737c31b23c7dade151a
Dave McCormick <dave@practicalclouds>
2013-04-13 14:36:51 +0100

    Update type to use newvalue methods and add wait and max_wait parameters

--- lib/puppet/provider/ec2instance/fog.rb
+++ lib/puppet/provider/ec2instance/fog.rb
@@ -14,32 +14,32 @@ Puppet::Type.type(:ec2instance).provide(:fog) do
 	def create
 		required_params = [ :name, :image_id, :min_count, :max_count ]
 		simple_params = { :instance_type => 'InstanceType', 
-								:key_name => 'KeyName', 
-								:kernel_id => 'KeyName', 
-								:ramdisk_id => 'RamdiskId', 
-								:monitoring_enabled => 'Monitoring.Enabled', 
-								:subnet_id => 'SubnetId', 
-								:private_ip_address => 'PrivateIpAddress', 
-								:disable_api_termination => 'DisableApiTermination', 
-								:ebs_optimized => 'EbsOptimized', 
-								:user_data => 'UserData' }
+			:key_name => 'KeyName', 
+			:kernel_id => 'KeyName', 
+			:ramdisk_id => 'RamdiskId', 
+			:monitoring_enabled => 'Monitoring.Enabled', 
+			:subnet_id => 'SubnetId', 
+			:private_ip_address => 'PrivateIpAddress', 
+			:disable_api_termination => 'DisableApiTermination', 
+			:ebs_optimized => 'EbsOptimized', 
+			:user_data => 'UserData' }
 		#complex_params = [ :security_group_names, :security_group_ids, :block_device_mapping ]
 		fog_options = {}
 
 		# check required parameters...
-  		required_params.each {|param|
-    		if (!@resource[param])
-      		notice "Missing required attribute #{param}!"
-      		raise "ec2instance[aws]->create: Sorry, you must include \"#{param}\" when defining an ec2instance!"
-    		end
-  		}
+		required_params.each {|param|
+			if (!@resource[param])
+				notice "Missing required attribute #{param}!"
+				raise "ec2instance[aws]->create: Sorry, you must include \"#{param}\" when defining an ec2instance!"
+			end
+		}
 		# copy simple parameters to the fog_options hash..
-  		simple_params.keys.each {|param|
-    		if (@resource[param])
+		simple_params.keys.each {|param|
+			if (@resource[param])
 				notice "Adding fog parameter #{simple_params[param].to_s} : #{@resource[param]}"
 				fog_options[simple_params[param].to_s]=@resource[param]		
-    		end
-  		}
+			end
+		}
 
 		# Work out region and connect to AWS...
 		if (@resource[:availability_zone]) then
@@ -53,12 +53,12 @@ Puppet::Type.type(:ec2instance).provide(:fog) do
 		# process the complex params which need processing into fog options...
 		# each option is implemented as it's own method which maps
 		# parameters into fog_options 
-  		#complex_params.each {|param|
-    	#	if (@resource[param])
+		#complex_params.each {|param|
+		#	if (@resource[param])
 		#		notice "ec2instance[aws]->create: processing parameter #{param}\n" if $debug
 		#		self.send(:"param_#{param}",compute,@resource,fog_options)
-    	#	end
-  		#}
+		#	end
+		#}
 
 		# start the instance
 		response = compute.run_instances(@resource[:image_id],@resource[:min_count].to_i,@resource[:max_count].to_i,fog_options)	
@@ -71,19 +71,27 @@ Puppet::Type.type(:ec2instance).provide(:fog) do
 			else
 				raise "ec2instance[aws]->create: I couldn't tag #{instid} with Name = #{@resource[:name]}, sorry! API Error!"
 			end
-			# Wait for my instance to start...
-         check = instanceinfo(compute,@resource[:name])
-			if ( check && check['instanceState']['name'] == "pending" )
-				notice "Waiting for #{@resource[:name]} tp start up..."
-         	while ( check['instanceState']['name'] != "running" ) do
-            	notice "ec2instance[aws]->create: #{@resource[:name]} is #{check['instanceState']['name']}\n" if $debug
-            	sleep 5
-            	check = instanceinfo(compute,@resource[:name])
-         	end
-         	sleep 5  # allow aws to propigate the fact
-         	notice "ec2instance[aws]->create: #{@resource[:name]} is #{check['instanceState']['name']}\n" if $debug
-			elsif (check && check['instanceState']['name'] != "running" )
-				raise "ec2instance[aws]->create: Sorry, #{@resource[:name]} is #{check['instanceState']['name']} and I expected it to be 'pending'"
+			if (@resource[:wait] == :true)
+				# Wait for my instance to start...
+				elapsed_wait=0
+				check = instanceinfo(compute,@resource[:name])
+				if ( check && check['instanceState']['name'] == "pending" )
+					notice "Waiting for #{@resource[:name]} tp start up..."
+					while ( check['instanceState']['name'] != "running" && elapsed_wait < @resource[:max_wait]  ) do
+						notice "ec2instance[aws]->create: #{@resource[:name]} is #{check['instanceState']['name']}\n" if $debug
+						sleep 5
+						elapsed_wait += 5
+						check = instanceinfo(compute,@resource[:name])
+					end
+					if (elapsed_wait >= @resource[:max_wait])
+						raise "ec2instance[aws]->create: Timed out waiting for #{@resource[:name]} to start up!"
+					else
+						sleep 5  # allow aws to propigate the fact
+						notice "ec2instance[aws]->create: #{@resource[:name]} is #{check['instanceState']['name']}\n" if $debug
+					end
+				elsif (check && check['instanceState']['name'] != "running" )
+					raise "ec2instance[aws]->create: Sorry, #{@resource[:name]} is #{check['instanceState']['name']} and I expected it to be 'pending'"
+				end
 			end
 		else
 			raise "ec2instance[aws]->create: I couldn't create the ec2instance, sorry! API Error!"
